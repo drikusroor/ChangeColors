2b61e5d61b6ac5d0a98ea382c12181ae702f55fc
Roor <droor@acto.nl>
2017-01-24 17:10:15 +0100

    Prepared the repo for github

--- change-colors.py
+++ change-colors.py
@@ -1,8 +1,7 @@
 from ImageGenerator import *
-from random_words import *
 import uuid
 
-def change_colors(source_file_name, amount_pictures):
+def change_colors(source_file_name, amount_pictures, variety):
     image = Image.open(source_file_name)
     rgb_values = convert_image_to_list(image)
     unique_rgb_values = get_unique_values_from_image_list(rgb_values)
@@ -10,17 +9,17 @@ def change_colors(source_file_name, amount_pictures):
     # Delete the background transparent layer from this list,
     # assuming the first pixel is transparent
 
-    # hsv_values = convert_list_to_hsv(convert_image_to_list(image))
-    # unique_hsv_values = get_unique_values_from_image_list(hsv_values)
-    # hsv_new = convert_list_to_rgb(change_color_skin(hsv_values, unique_hsv_values, -90, 0, +.1, 'HSV'))
-
-
     for i in range(0, amount_pictures):
-        rand_r = random.randint(-100, 100)
-        rand_g = random.randint(-100, 100)
-        rand_b = random.randint(-100, 100)
+        rand_r = random.randint(-variety, variety)
+        rand_g = random.randint(-variety, variety)
+        rand_b = random.randint(-variety, variety)
         rgb_new = change_color_skin(rgb_values, unique_rgb_values, rand_r, rand_g, rand_b, 'RGB')
         random_word = str(uuid.uuid1())
         make_image(rgb_new, random_word)
 
-# change_colors('FeetAnim1-sheet.png', 10)
+# To use, simply use the following command
+# First param is the filename as string
+# Second param is the amount of random images that will be created
+# Third param is the amount of variety in color
+# that can occur in each of the channels
+# change_colors('FeetAnim1-sheet.png', 10, 200)
